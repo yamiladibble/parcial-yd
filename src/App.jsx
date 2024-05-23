e083878a99b6133b32fc4f36fc4db6bdf91b505b
Yamila <yamiladibble.20@gmail.com>
2024-05-23 18:20:38 +0100

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Card from "./Componentes/Card";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+}
+
+
+export default App;
+
+
+
